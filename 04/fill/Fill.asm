eb500ed0ccf111be38b5895c390998ad45925759
Rams01010010 <ramsolaiappan@gmail.com>
2022-09-24 17:54:14 +0530

    Completed Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,66 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//Initialize Constants
+@KBD
+D=A
+@SCREEN
+D=D-A
+@n
+M=D
+
+//Infinite Loop for detecting keyboard
+(START)
+@KBD  //Keyboard Register
+D=M
+@WHITE
+D;JEQ   //Jump to White if the register value is 0
+
+(BLACK)
+@i
+M=0
+
+(BLOOP) //Loop for producing black screen
+
+@i
+D=M
+@n
+D=D-M
+
+@START
+D;JEQ
+
+@SCREEN
+D=A
+@i
+A=D+M
+M=-1
+@i
+M=M+1
+
+@BLOOP
+0;JMP
+
+(WHITE) //Loop for producing white screen
+@i
+M=0
+
+(WLOOP)
+@i
+D=M
+@n
+D=D-M
+
+@START
+D;JEQ
+
+@SCREEN
+D=A
+@i
+A=D+M
+M=0
+@i
+M=M+1
+
+@WLOOP
+0;JMP
